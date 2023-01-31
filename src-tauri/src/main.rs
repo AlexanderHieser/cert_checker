44fb942e045b401d5f01c8e3ff20ec0daf420a39
Alexander Hieser <Alexander.Hieser@online.de>
2023-01-31 20:08:39 +0100

    Add Tailwind, add basic ui test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,9 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{Manager, Size, LogicalSize};
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,7 +14,19 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let main_window = app.get_window("main").unwrap();
+            println!("Initializing");
+            
+            main_window.set_size(Size::Logical(LogicalSize {
+                width: 350.0,
+                height: 570.0
+            })).unwrap();
+            main_window.set_resizable(false).unwrap();
+            println!("Done set size");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
+    }
--- src/src/App.vue
+++ src/src/App.vue
@@ -1,29 +1,37 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="p-2 m-2">
+    <div class="content-center h-28 bg-gray-50 rounded-md border-solid border-2 border-gray-500 border-dashed">
+      <p class="text-center pt-11 font-bold text-slate-400">Drop File Here</p>
+    </div>
+    <div class="m-full">
+      <label class="block text-left pt-2 text-gray-800 text-sm font-bold mb-1" for="username">
+        Target Path
+      </label>
+      <div class="bg-slate-400 shadow-md rounded px-3 pt-4 pb-1 ">
+        <div class="mb-2">
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="blake" type="text" placeholder="../../">
+          <button class="bg-blue-500 rounded-md text-white text-lg font-bold mt-2 w-full">Load File</button>
+        </div>
+      </div>
+      <label class="block text-left text-gray-800  font-bold mb-1 pt-2" for="username">
+        Result
+      </label>
+      <div
+        class="bg-slate-300 text-gray-600 max-h-56 overflow-auto text-white pb-4 h-fullshadow-md rounded px-3 pt-4 pb-1 mt-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt harum quis placeat rem, ratione, voluptates nisi
+        deleniti mollitia voluptatem accusantium aut dolor nesciunt dolorem. Asperiores minus cum pariatur fugit
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptate maiores officiis impedit nemo
+        voluptates repudiandae assumenda optio, ipsam ipsa, necessitatibus possimus nobis. Maiores quia, ex earum
+        nesciunt neque magni.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima nisi in enim eligendi molestias unde.
+        Blanditiis incidunt tenetur enim eaque nisi sed harum amet? Aperiam maiores tempora quas quibusdam placeat!
+        eligendi!
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
