ebd990c3333ac41e6f1736d873984d71d34211ea
Alexander Hieser <Alexander.Hieser@online.de>
2023-08-09 20:05:07 +0200

    Remove oruga imports

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,9 +1,4 @@
 import { createApp  } from 'vue'
 import './style.css'
 import App from './App.vue'
-import Oruga from '@oruga-ui/oruga-next'
-import '@oruga-ui/oruga-next/dist/oruga.css'
-import { bulmaConfig } from '@oruga-ui/theme-bulma'
-
-import '@oruga-ui/theme-bulma/dist/bulma.css'
-createApp(App).use(Oruga,bulmaConfig).mount('#app')
+createApp(App).mount('#app')
